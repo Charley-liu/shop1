038cb3b250d768c26d3019a89c90c511d528edbf
Travis <travis@travis-ci.org>
2018-11-05 13:43:24 +0800

    购物车编写

--- src/pages/counter/counter.vue
+++ src/pages/counter/counter.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="counter-warp">
+    <div class="shop_car_title">
+      <span class="shop_car_titlename margin-left">微商城</span><span class="shop_car_titleoption margin-right">完成</span>
+    </div>
     <span class="weui-badge">New</span>
     <p>Vuex counter：{{ count }}</p>
     <p>
@@ -32,7 +35,6 @@ export default {
 <style>
 .counter-warp {
   text-align: center;
-  margin-top: 100px;
 }
 .home {
   display: inline-block;
@@ -40,5 +42,27 @@ export default {
   padding: 5px 10px;
   color: blue;
   border: 1px solid blue;
+}
+.counter-warp{
+  background-color: #f8f8f8;
+}
+.shop_car_title{
+  height:50px;
+}
+.shop_car_titlename{
+  float: left;
+}
+.shop_car_titleoption{
+  float: right;
+}
+.margin-left{
+  margin-left:15px;
+  font: 0.75em sans-serif;
+  line-height: 50px;
+}
+.margin-right{
+  margin-right: 15px;
+  font: 0.75em sans-serif;
+  line-height: 50px;
 }
 </style>
